ac3e0e1eab82bfb5c98536b24676ed96e1b2c176
Kim Valkama <hello@valkama.kim>
2026-01-26 00:37:54 +0200

    Updated, multi parser broken for now

--- log_parser.pl
+++ log_parser.pl
@@ -22,10 +22,14 @@ close($fh);
 
 # Check mode and decide which script to run.
 if ($config{'mode'} eq 'single_line') {
-    my $args = join(' ', map { "-v $_=\"$config{$_}\"" } grep { $_ ne 'mode' } keys %config);
+    my $args = join(' ', map { "$_=\"$config{$_}\"" } grep { $_ ne 'mode' } keys %config);
+    my $file_to_parse = $ARGV[0] // die "Error: Please provide a file to parse as the first argument.";
+    $args .= " $file_to_parse";
     system("perl parsers/single_line.awk $args");
 } elsif ($config{'mode'} eq 'multi_line') {
     my $args = join(' ', map { "--$_=$config{$_}" } grep { $_ ne 'mode' } keys %config);
+    my $file_to_parse = $ARGV[0] // die "Error: Please provide a file to parse as the first argument.";
+    $args .= " $file_to_parse";
     system("perl parsers/multi_line.awk $args");
 } else {
     die "Error: mode '$config{mode}' is not recognized. Please use 'mode=single_line' or 'mode=multi_line'.\n";
