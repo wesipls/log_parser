dc35192bf4ebff877a84199dacdbc769d2bd97c3
Kim Valkama <hello@valkama.kim>
2026-01-28 20:48:40 +0200

    added loop for directories

--- log_parser.pl
+++ log_parser.pl
@@ -25,16 +25,36 @@ while (my $line = <$fh>) {
 }
 close($fh);
 
-if ($config{'mode'} eq 'single_line') {
-    my $args = join(' ', map { "-v $_=\"$config{$_}\"" } grep { $_ ne 'mode' } keys %config);
-    $args .= " $file_to_parse";
-    system("perl parsers/single_line.awk $args");
-} elsif ($config{'mode'} eq 'multi_line') {
-    my $args = join(' ', map { "-v $_=\"$config{$_}\"" } grep { $_ ne 'mode' } keys %config);
-    $args .= " $file_to_parse";
-    system("perl parsers/multi_line.awk $args");
+if (-d $file_to_parse) {
+    opendir(my $dh, $file_to_parse) or die "Cannot open directory $file_to_parse: $!";
+    while (my $file = readdir($dh)) {
+        next if ($file =~ /^\./); # Skip hidden files and directories (e.g., ., ..)
+        my $full_path = "$file_to_parse/$file";
+        next unless (-f $full_path); # Process only if it's a file
+
+        if ($config{'mode'} eq 'single_line') {
+            my $args = join(' ', map { "-v $_=\"$config{$_}\"" } grep { $_ ne 'mode' } keys %config);
+            $args .= " $full_path";
+            system("perl parsers/single_line.awk $args");
+        } elsif ($config{'mode'} eq 'multi_line') {
+            my $args = join(' ', map { "-v $_=\"$config{$_}\"" } grep { $_ ne 'mode' } keys %config);
+            $args .= " $full_path";
+            system("perl parsers/multi_line.awk $args");
+        }
+    }
+    closedir($dh);
 } else {
-    die "Error: mode '$config{mode}' is not recognized. Please use 'mode=single_line' or 'mode=multi_line'.\n";
+    if ($config{'mode'} eq 'single_line') {
+        my $args = join(' ', map { "-v $_=\"$config{$_}\"" } grep { $_ ne 'mode' } keys %config);
+        $args .= " $file_to_parse";
+        system("perl parsers/single_line.awk $args");
+    } elsif ($config{'mode'} eq 'multi_line') {
+        my $args = join(' ', map { "-v $_=\"$config{$_}\"" } grep { $_ ne 'mode' } keys %config);
+        $args .= " $file_to_parse";
+        system("perl parsers/multi_line.awk $args");
+    } else {
+        die "Error: mode '$config{mode}' is not recognized. Please use 'mode=single_line' or 'mode=multi_line'.\n";
+    }
 }
 
 
